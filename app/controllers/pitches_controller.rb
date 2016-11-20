ce707d7dec02947d19478314754828653fbfb12a
Zack Rowden <email": null>
2016-11-19 22:14:52 -0500

    Create user checkboxes create submit button for votes, does not route correctly.

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,5 +1,5 @@
 class PitchesController < ApplicationController
-
+  before_action :sanitize_params, only: [:create]
   def index
   end
 
@@ -7,30 +7,15 @@ class PitchesController < ApplicationController
   end
 
   def create
-
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-      puts "================"
-
+    binding.pry
     @pitch = Pitch.new(pitch_params)
     if @pitch.save
-
-      render 'new'
+      redirect_to :back
     end
   end
 
   def edit
+
   end
 
   def update_index
@@ -38,15 +23,22 @@ class PitchesController < ApplicationController
   end
 
   def update
+
   end
 
   def destroy
+    @pitch.update_attributes(pitch_params)
   end
 
   private
 
   def pitch_params
-    params.require(:pitch).permit(:working_title, :description)
+    params.require(:pitch).permit(:working_title, :description, :user_id, :round_id)
+  end
+
+  def sanitize_params
+    params[:pitch][:round_id] = Round.last.id
+    params[:pitch][:user_id] = current_user.id
   end
 
 
