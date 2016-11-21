b4560c18bb6b5823f273510e0a95faf5ebdf9b13
Catherine Khuu <sunshine.dumbo@gmail.com>
2016-11-21 09:38:10 -0500

    Can scroll to view page.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,21 @@
 class VotesController < ApplicationController
 
-  def create
+  def new
+    @vote = Vote.new
   end
 
+  def create
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      flash[:notice] = "thank you for voting"
+    else
+      flash.now[:danger] = "vote did not save"
+    end
+  end
+
+  private
+    def vote_params
+      binding.pry
+      params.require(:vote).permit(:user_id, :pitch_id, :round_id)
+    end
 end
