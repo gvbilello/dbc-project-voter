5173a1a093173abf05722d33dd2f1e8e4ca702c8
Gregory Bilello <email": null>
2016-11-21 13:45:21 -0500

    Fix merge confllicts and adjusted CSS for admin/user show. Jquery to disable buttons.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,17 +5,31 @@ class VotesController < ApplicationController
   end
 
   def create
-    @vote = Vote.new(vote_params)
-    if @vote.save
-      flash[:notice] = "thank you for voting"
+    # binding.pry
+    if params["commit"] == "Final Vote Rankings"
+      @vote = Vote.new(vote_ranking_params)
+      if @vote.save
+        flash[:notice] = "thank you for submitting your rankings"
+      else
+        flash[:danger] = "rankings not saved"
+      end
     else
-      flash.now[:danger] = "vote did not save"
+      @vote = Vote.new(vote_params)
+      if @vote.save
+        flash[:notice] = "thank you for voting"
+      else
+        flash.now[:danger] = "vote did not save"
+      end
     end
   end
 
   private
     def vote_params
-      binding.pry
+      # binding.pry
       params.require(:vote).permit(:user_id, :pitch_id, :round_id)
+    end
+
+    def vote_ranking_params
+      params.require(:vote).permit(:user_id, :pitch_id, :round_id, :ranking)
     end
 end
